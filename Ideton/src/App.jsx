e4ac9c95b56837f0ba9c06464529542422311296
KarpX <carpeev.maksimka@yandex.ru>
2026-02-04 18:18:30 +0300

    note delete func., edit page, cool btns

--- Ideton/src/App.jsx
+++ Ideton/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, set } from "date-fns";
 import { ru } from "date-fns/locale";
 import "./App.css";
 
@@ -8,19 +8,64 @@ function App() {
   const [content, setContent] = useState("");
 
   const [notes, setNotes] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedNoteId, setSelectedNoteId] = useState(null);
 
-  const handleSave = () => {
-    if (!content.trim()) return;
-
-    const newNote = {
-      id: Date.now(),
-      title: title.trim() === "" ? "New note" : title,
-      content: content.trim(),
-    };
-
-    setNotes([newNote, ...notes]);
+  const createNewNote = () => {
+    setIsEditing(true);
+    setSelectedNoteId(null);
     setTitle("");
     setContent("");
+  };
+
+  const selectNote = (note) => {
+    setIsEditing(true);
+    setSelectedNoteId(note.id);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+  const deleteNote = (noteId) => {
+    setNotes(notes.filter((n) => n.id !== noteId));
+
+    if (selectedNoteId === noteId) {
+      setIsEditing(false);
+      setSelectedNoteId(null);
+      setTitle("");
+      setContent("");
+    }
+  };
+
+  const handleSave = () => {
+    if (!content.trim()) {
+      setIsEditing(false);
+      return;
+    }
+
+    if (selectedNoteId) {
+      setNotes(
+        notes.map((n) =>
+          n.id === selectedNoteId
+            ? {
+                ...n,
+                title: title.trim() === "" ? n.title : title,
+                content: content.trim(),
+                id: Date.now(),
+              }
+            : n,
+        ),
+      );
+      setIsEditing(false);
+    } else {
+      const newNote = {
+        id: Date.now(),
+        title: title.trim() === "" ? "New note" : title,
+        content: content.trim(),
+      };
+      setNotes([newNote, ...notes]);
+
+      setIsEditing(false);
+    }
   };
 
   return (
@@ -29,38 +74,68 @@ function App() {
         <h1>IDETON</h1>
         <div className="field">
           <div className="menu-panel">
-            <p>menu panel</p>
+            <button className="add-note-btn" onClick={createNewNote}>
+              <span>+</span>
+            </button>
           </div>
           <div className="note-field">
-            {notes.map((note) => (
-              <div key={note.id} className="note">
-                <h3>{note.title}</h3>
-                <p>{note.content}</p>
-                <footer className="note-time">
-                  {formatDistanceToNow(new Date(note.id), {
-                    addSuffix: true,
-                    locale: ru,
-                  })}
-                </footer>
+            {isEditing ? (
+              <div className="editor-area">
+                <input
+                  className="title-input"
+                  placeholder="Title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                ></input>
+                <textarea
+                  className="note-text"
+                  placeholder="Type here..."
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                <div className="button-container">
+                  <button
+                    className="cancel-button"
+                    onClick={() => {
+                      setIsEditing(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button className="save-button" onClick={handleSave}>
+                    Save
+                  </button>
+                </div>
               </div>
-            ))}
-          </div>
-          <div className="editor-area">
-            <input
-              className="title-input"
-              placeholder="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            ></input>
-            <textarea
-              className="note-text"
-              placeholder="Type here..."
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            ></textarea>
-            <button className="save-button" onClick={handleSave}>
-              Save
-            </button>
+            ) : (
+              <div className="note-list">
+                {notes.map((note) => (
+                  <div
+                    key={note.id}
+                    className="note"
+                    onClick={() => selectNote(note)}
+                  >
+                    <button
+                      className="delete-btn"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        if (window.confirm("Delete note?")) deleteNote(note.id);
+                      }}
+                    >
+                      🗑
+                    </button>
+                    <h3>{note.title}</h3>
+                    <p>{note.content}</p>
+                    <footer className="note-time">
+                      {formatDistanceToNow(new Date(note.id), {
+                        addSuffix: true,
+                        locale: ru,
+                      })}
+                    </footer>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
