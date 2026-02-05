056269e2bc42552432c53cccbb99f51a47ede2eb
KarpX <carpeev.maksimka@yandex.ru>
2026-02-05 22:54:00 +0300

    folder btn on note hover

--- Ideton/src/App.jsx
+++ Ideton/src/App.jsx
@@ -239,13 +239,34 @@ function App() {
                     onClick={() => selectNote(note)}
                   >
                     <button
-                      className="delete-btn"
+                      className="delete-btn on-note-btn"
                       onClick={(e) => {
                         e.stopPropagation();
                         if (window.confirm("Delete note?")) deleteNote(note.id);
                       }}
                     >
                       🗑
+                    </button>
+                    <button
+                      className="folder-btn on-note-btn"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        console.log("clicked");
+                      }}
+                    >
+                      <svg
+                        width="30"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="folder-icon"
+                      >
+                        <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+                      </svg>
                     </button>
                     <h3>{note.title}</h3>
                     <p>{note.content}</p>
